13ac8c665b794e3494db78e57c0f1f615bd0190d
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 16:52:48 +0300

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -1,14 +1,6 @@
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import SUPPORT_GROUP, SUPPORT_CHANNEL
 import random
-
-from pyrogram.types import InlineKeyboardButton
 
 selections = [
     "ــــــــــــــــــــ★ـــ",
