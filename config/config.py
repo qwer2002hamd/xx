4366fcf2dec1c81691c030c997349b039b26f898
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-05 00:36:00 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -46,6 +46,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ah07v")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ah07v")
+MUST_JOIN = getenv("MUST_JOIN", "ah07v")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
