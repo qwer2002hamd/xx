a73abd27240052620b23b0746c45b2a4d4124c90
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-05 22:44:35 +0300

    Update pause.py

--- AlexaMusic/plugins/admins/pause.py
+++ AlexaMusic/plugins/admins/pause.py
@@ -24,7 +24,28 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
 @app.on_message(
-    filters.command(PAUSE_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+ filters.command(PAUSE_COMMAND,"")
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"], disable_web_page_preview=True)
+    await music_off(chat_id)
+    await Alexa.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention), disable_web_page_preview=True
+    )
+
+
+@app.on_message(
+ filters.command(PAUSE_COMMAND,"")
+    & ~filters.edited
+    & filters.channel
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
